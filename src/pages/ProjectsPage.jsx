2f8f723d429472a46d0a77550f5a1efe59186b27
mark2515 <hekunlong2515@gmail.com>
2025-08-25 16:00:55 -0700

    Update the projects page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,13 +1,14 @@
 import { useEffect } from 'react';
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaVuejs } from 'react-icons/fa';
-import { SiExpress, SiCreatereactapp, SiNextdotjs, SiTypescript, SiMongodb, SiSpringboot, SiMysql, SiApachemaven, SiLangchain, SiTailwindcss } from "react-icons/si";
+import { SiExpress, SiCreatereactapp, SiNextdotjs, SiTypescript, SiMongodb, SiSpringboot, SiMysql, SiApachemaven, SiLangchain, SiTailwindcss, SiPython, SiPandas, SiScikitlearn, SiPytorch } from "react-icons/si";
 import { ReactSVG } from 'react-svg';
 import Title from '../components/Title';
+import chatbot from '../assets/chatbot.jpg';
 import monopoly from '../assets/monopoly.jpg';
-import shopway from '../assets/shopway.jpg';
 import battleship from '../assets/battleship.png';
 import cms from '../assets/cms.jpg';
-import chatbot from '../assets/chatbot.jpg';
+import shopway from '../assets/shopway.jpg';
+import fhc from '../assets/fhc.jpg';
 import chatGPT from '../assets/ChatGPT.svg';
 
 import AOS from 'aos';
@@ -39,24 +40,33 @@ const Projects = () => {
         <div className='card-wrapper h-[32rem] w-[20rem] hover:scale-105 transition ease-in-out mt-20'>
           <div className='card-content'>
               <div className="relative h-56">
-                <img src={shopway} alt="Shopway" className="object-cover w-full h-full rounded-t-2xl" />
+                <img src={chatbot} alt="Chatbot" className="object-cover w-full h-full rounded-t-2xl" />
               </div>
               <div className="px-6 py-4">
-                <div className="font-bold text-xl mt-2">ShopWay Online Mall</div>
+                <div className="font-bold text-xl mt-2">Corpus AI Coach</div>
                 <p className="text-slate-500 text-base mt-2">
-                  An online mall built on the MERN stack with RESTful APIs, offering a scalable architecture and an intuitive user interface for efficient e-commerce functionality.
+                  An English learning platform that provides high-frequency word alerts and integrates a GPT-powered chatbot for accurate translation and conversational practice.
                 </p>
               </div>
               <div className="flex mt-3">
-                <a href="https://shopway-4c77d3b0a927.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.corpusaicoach.com/" target="_blank" rel="noopener noreferrer">
                   <button className="bg-blue-500 hover:bg-blue-700 hover:transition hover:ease-in-out text-white font-normal py-2 px-4 ml-5 rounded transition ease-in-out">
                     View Site
                   </button></a>
-                  <div className="grid grid-cols-4 ml-4 mt-1">
+                  <div className="grid grid-cols-4 ml-4 -mt-3">
                     <IconWithTooltip icon={SiMongodb} label="MongoDB" />
                     <IconWithTooltip icon={SiExpress} label="Express.js" />
                     <IconWithTooltip icon={FaReact} label="React" />
-                    <IconWithTooltip icon={FaNodeJs} label="Node.js" />
+                    <IconWithTooltip icon={SiNextdotjs} label="Next.js" />
+                    <IconWithTooltip icon={SiTypescript} label="TypeScript" />
+                    <IconWithTooltip icon={SiTailwindcss} label="Tailwind CSS" />
+                    <IconWithTooltip icon={SiLangchain} label="Langchain" />
+                    <div className="relative group flex items-center mx-1">
+                      <ReactSVG src={chatGPT} className="text-3xl mb-3 group-hover:text-blue-500 transition-colors" />
+                      <span className="absolute left-1/2 transform -translate-x-1/2 mt-8 w-max text-white text-xs font-medium p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity">
+                        OpenAI API
+                      </span>
+                    </div>
                   </div>
               </div>
           </div>
@@ -70,7 +80,7 @@ const Projects = () => {
               <div className="px-6 py-4">
                 <div className="font-bold text-xl mt-2">Battleship</div>
                 <p className="text-slate-500 text-base mt-2">
-                  A team-developed Battleship game, featuring AI algorithms and turn-based management to provide an engaging and challenging player experience.
+                  Our team's Battleship game features AI algorithms and turn-based management to provide an engaging player experience and smooth gameplay logic.
                 </p>
               </div>
               <div className="flex mt-3">
@@ -96,7 +106,7 @@ const Projects = () => {
               <div className="px-6 py-4">
                 <div className="font-bold text-xl mt-2">Single-Player Monopoly</div>
                 <p className="text-slate-500 text-base mt-2">
-                  A single-player Monopoly game developed with JavaScript, HTML, and CSS, implementing immersive gameplay features and smooth game logic.
+                  A React-based single-player Monopoly game where you buy goods, upgrade properties, and manage assets to reach the victory goal.
                 </p>
               </div>
               <div className="flex mt-3">
@@ -113,43 +123,7 @@ const Projects = () => {
               </div>
           </div>
         </div>
-
-        <div className='card-wrapper h-[32rem] w-[20rem] hover:scale-105 transition ease-in-out mt-20'>
-          <div className='card-content'>
-              <div className="relative h-56">
-                <img src={chatbot} alt="Chatbot" className="object-cover w-full h-full rounded-t-2xl" />
-              </div>
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mt-2">Language Learn Chatbot</div>
-                <p className="text-slate-500 text-base mt-2">
-                  An English learning platform that provides high-frequency word alerts and integrates a GPT-powered chatbot for accurate translations and practical language practice.
-                </p>
-              </div>
-              <div className="flex mt-3">
-                <a href="https://github.com/mark2515/language-learn-chatbot" target="_blank" rel="noopener noreferrer">
-                  <button className="bg-blue-500 hover:bg-blue-700 hover:transition hover:ease-in-out text-white font-normal py-2 px-4 ml-5 rounded transition ease-in-out">
-                    Read More
-                  </button></a>
-                  <div className="grid grid-cols-4 ml-4 -mt-3">
-                    <IconWithTooltip icon={SiMongodb} label="MongoDB" />
-                    <IconWithTooltip icon={SiExpress} label="Express.js" />
-                    <IconWithTooltip icon={FaReact} label="React" />
-                    <IconWithTooltip icon={SiNextdotjs} label="Next.js" />
-                    <IconWithTooltip icon={SiTypescript} label="TypeScript" />
-                    <IconWithTooltip icon={SiTailwindcss} label="Tailwind CSS" />
-                    <IconWithTooltip icon={SiLangchain} label="Langchain" />
-                    <div className="relative group flex items-center mx-1">
-                      <ReactSVG src={chatGPT} className="text-3xl mb-3 group-hover:text-blue-500 transition-colors" />
-                      <span className="absolute left-1/2 transform -translate-x-1/2 mt-8 w-max text-white text-xs font-medium p-1 rounded opacity-0 group-hover:opacity-100 transition-opacity">
-                        ChatGPT APIs
-                      </span>
-                    </div>
-
-                  </div>
-              </div>
-          </div>
-        </div>
-
+       
         <div className='card-wrapper h-[32rem] w-[20rem] hover:scale-105 transition ease-in-out mt-20'>
           <div className='card-content'>
               <div className="relative h-56">
@@ -171,6 +145,58 @@ const Projects = () => {
                     <IconWithTooltip icon={SiSpringboot} label="SpringBoot" />
                     <IconWithTooltip icon={SiApachemaven} label="Maven" />
                     <IconWithTooltip icon={SiMysql} label="MySQL" />
+                  </div>
+              </div>
+          </div>
+        </div>
+
+        <div className='card-wrapper h-[32rem] w-[20rem] hover:scale-105 transition ease-in-out mt-20'>
+          <div className='card-content'>
+              <div className="relative h-56">
+                <img src={shopway} alt="Shopway" className="object-cover w-full h-full rounded-t-2xl" />
+              </div>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mt-2">ShopWay Online Mall</div>
+                <p className="text-slate-500 text-base mt-2">
+                  An online mall built on the MERN stack with RESTful APIs, offering an intuitive user interface and a scalable architecture for efficient e-commerce functionality.
+                </p>
+              </div>
+              <div className="flex mt-3">
+                <a href="https://github.com/mark2515/shopway-online-mall" target="_blank" rel="noopener noreferrer">
+                  <button className="bg-blue-500 hover:bg-blue-700 hover:transition hover:ease-in-out text-white font-normal py-2 px-4 ml-5 rounded transition ease-in-out">
+                    Read More
+                  </button></a>
+                  <div className="grid grid-cols-4 ml-4 mt-1">
+                    <IconWithTooltip icon={SiMongodb} label="MongoDB" />
+                    <IconWithTooltip icon={SiExpress} label="Express.js" />
+                    <IconWithTooltip icon={FaReact} label="React" />
+                    <IconWithTooltip icon={FaNodeJs} label="Node.js" />
+                  </div>
+              </div>
+          </div>
+        </div>
+
+        <div className='card-wrapper h-[32rem] w-[20rem] hover:scale-105 transition ease-in-out mt-20'>
+          <div className='card-content'>
+              <div className="relative h-56">
+                <img src={fhc} alt="CTG" className="object-cover w-full h-full rounded-t-2xl" />
+              </div>
+              <div className="px-6 py-4">
+                <div className="font-bold text-lg mt-2">Fetal Health Classification</div>
+                <p className="text-slate-500 text-base mt-2">
+                  Our ML-based fetal health classification method using Cardiotocogram (CTG) data to predict normal, suspect, and pathological conditions for early intervention.
+                </p>
+              </div>
+              <div className="flex mt-3">
+                <a href="https://github.com/mark2515/fetal-health-classification" target="_blank" rel="noopener noreferrer">
+                  <button className="bg-blue-500 hover:bg-blue-700 hover:transition hover:ease-in-out text-white font-normal py-2 px-4 ml-5 rounded transition ease-in-out">
+                    Read More
+                  </button></a>
+                  <div className="grid grid-cols-4 ml-4 mt-1">
+                    <IconWithTooltip icon={SiPython} label="Python" />
+                    <IconWithTooltip icon={SiPandas} label="Pandas" />
+                    <IconWithTooltip icon={SiScikitlearn} label="Scikit-learn" />
+                    <IconWithTooltip icon={SiPytorch} label="Pytorch" />
                   </div>
               </div>
           </div>
